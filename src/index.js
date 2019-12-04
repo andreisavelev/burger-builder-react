d162b662937d466de00be3b59258ed594d365d29
Савельев Андрей Александрович <savelevcorr@gmail.com>
2019-12-04 15:44:38 +0300

    Add google fonts loading

--- src/index.js
+++ src/index.js
@@ -4,5 +4,20 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// load goolge font OpenSans with cyrillic extention
+window.WebFontConfig = {
+    google: { families: [ 'Open+Sans::cyrillic-ext' ] }
+};
+(function() {
+    const wf = document.createElement('script');
+    const s = document.getElementsByTagName('script')[0];
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    s.parentNode.insertBefore(wf, s);
+})();
+
+// Render react app
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
