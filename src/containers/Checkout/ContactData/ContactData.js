8ba4121042e67a8729f367872376fc2434a5cd56
Andrew Savelev <savelevcorr@gmail.com>
2020-02-12 16:10:46 +0300

    Configure elemnts of form
    - Display inputs
    - Display selects
    - Display Textareas

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,14 +4,69 @@ import axios from '../../../axios-orders';
 import Button from '../../../components/ui/Button/Button';
 import classes from './ContactData.css';
 import Spinner from '../../../components/ui/Spinner/Spinner';
+import Input from '../../../components/ui/Input/Input';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    placeholder: 'Please, enter your name',
+                    type: 'text'
+                },
+                value: '',
+            },
+
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    placeholder: 'Please, enter your address',
+                    type: 'text'
+                },
+                value: ''
+            },
+
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    placeholder: 'Please, enter your zip code',
+                    type: 'number'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    placeholder: 'Please, enter your country',
+                    type: 'string'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    placeholder: 'Please, enter your email',
+                    type: 'email'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {
+                            value: 'fastest',
+                            displayValue: 'Fastest'
+                        },
+                        {
+                            value: 'cheapest',
+                            displayValue: 'Cheapest'
+                        }
+                    ]
+                },
+                value: ''
+            }
         },
 
         loading: false
@@ -52,17 +107,25 @@ class ContactData extends Component {
     };
 
     render() {
+        const orderFormArray = [];
+
+        for (let key in this.state.orderForm) {
+            if (this.state.orderForm.hasOwnProperty(key)) {
+                orderFormArray.push({
+                    id: key,
+                    config: this.state.orderForm[key]
+                });
+            }
+        }
+
         let form = (
             <form>
-                <input type="text"
-                       name="name"
-                       placeholder="Your Name"/>
-                <input type="email"
-                       name="email"
-                       placeholder="Your Email"/>
-                <input type="number"
-                       name="postal"
-                       placeholder="Your Postal Code"/>
+                {orderFormArray.map(item => (
+                    <Input key={item.id}
+                           elementType={item.config.elementType}
+                           elementConfig={item.config.elementConfig}
+                           value={item.config.value}/>
+                ))}
                 <Button btnType={'Success'}
                         clicked={this.orderHandler}>
                     ORDER
