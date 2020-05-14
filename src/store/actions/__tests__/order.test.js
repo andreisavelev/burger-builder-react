ffed060d169ed7c9699fc4bbcae4185ccc043382
Andrew Savelev <savelevcorr@gmail.com>
2020-05-14 17:43:59 +0300

    Add tests for purchaseBurgerSuccess and purchaseBurgerFail action creators

--- src/store/actions/__tests__/order.test.js
+++ src/store/actions/__tests__/order.test.js
@@ -1,8 +1,32 @@
-import { purchaseInit } from "../order";
-import { PURCHASE_INIT } from "../actionTypes";
+import {
+  purchaseInit,
+  purchaseBurgerSuccess,
+  purchaseBurgerFail,
+} from "../order";
+import {
+  PURCHASE_INIT,
+  PURCHASE_BURGER_SUCCESS,
+  PURCHASE_BURGER_FAIL,
+} from "../actionTypes";
 
 describe("Order actions", () => {
   it("should contains type PURCHASE_INIT", () => {
     expect(purchaseInit().type).toEqual(PURCHASE_INIT);
+  });
+
+  it("should returns correct order data from purchaseBurgerSuccess action creator", () => {
+    expect(purchaseBurgerSuccess("1", { test: "test" })).toEqual({
+      orderId: "1",
+      type: PURCHASE_BURGER_SUCCESS,
+      orderData: { test: "test" },
+    });
+  });
+
+  it("should returns correct type and error data", () => {
+    const error = { test: "test" };
+    expect(purchaseBurgerFail(error)).toEqual({
+      type: PURCHASE_BURGER_FAIL,
+      error,
+    });
   });
 });
